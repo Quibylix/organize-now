94dc800692471b846cb712a31cae418c5446bac2
Vanaldito <barraza.fredimanuel@gmail.com>
2023-11-01 14:06:23 -0600

    refactor: create the Icon component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { Icon } from "@/components";
+</script>
+
 <template>
   <main class="main">
     <img
@@ -16,12 +20,7 @@
     <a class="start" href="#">
       Get Started
       <span class="icon-container">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" viewBox="0 0 24 24">
-          <path
-            fill="currentColor"
-            d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6l-6 6Z"
-          />
-        </svg>
+        <Icon name="right-arrow" />
       </span>
     </a>
   </main>
---
+++ src/components/Icon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: "right-arrow" }>();
+</script>
+
+<template>
+  <svg
+    v-if="name === 'right-arrow'"
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6l-6 6Z"
+    />
+  </svg>
+</template>
