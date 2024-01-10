01b191a238f79d89568556a1b243c3bf302a0c17
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-10 09:52:30 -0600

    chore: add exclude pattern for e2e tests in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+    exclude: [...configDefaults.exclude, "e2e/**/*"],
   },
   resolve: {
     alias: {
