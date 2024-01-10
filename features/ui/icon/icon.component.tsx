6b858723d177022973d4c6bcbd28bfdf81816fb4
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-09 22:16:30 -0600

    feat: add UI icons components

---
+++ features/ui/icon/icon.component.tsx
@@ -0,0 +1,24 @@
+import CalendarIcon from "./calendar.component";
+import ClockIcon from "./clock.component";
+import HomeIcon from "./home.component";
+import PlusIcon from "./plus.component";
+import ProfileIcon from "./profile.component";
+
+type IconProps = {
+  name: "home" | "calendar" | "clock" | "profile" | "plus";
+} & React.SVGProps<SVGSVGElement>;
+
+export default function Icon({ name, ...props }: IconProps) {
+  switch (name) {
+    case "home":
+      return <HomeIcon {...props} />;
+    case "calendar":
+      return <CalendarIcon {...props} />;
+    case "clock":
+      return <ClockIcon {...props} />;
+    case "profile":
+      return <ProfileIcon {...props} />;
+    case "plus":
+      return <PlusIcon {...props} />;
+  }
+}
