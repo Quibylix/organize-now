ca4793a31d96ba80abe87a52204620a3b243a3b2
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-11 12:30:25 -0600

    feat: add eye and eye-off icons

--- features/ui/icon/icon.component.tsx
+++ features/ui/icon/icon.component.tsx
@@ -1,11 +1,13 @@
 import CalendarIcon from "./calendar.component";
 import ClockIcon from "./clock.component";
+import { EyeOffIcon } from "./eye-off.component";
+import { EyeIcon } from "./eye.component";
 import HomeIcon from "./home.component";
 import PlusIcon from "./plus.component";
 import ProfileIcon from "./profile.component";
 
 export type IconProps = {
-  name: "home" | "calendar" | "clock" | "profile" | "plus";
+  name: "home" | "calendar" | "clock" | "profile" | "plus" | "eye" | "eye-off";
 } & React.SVGProps<SVGSVGElement>;
 
 export default function Icon({ name, ...props }: IconProps) {
@@ -20,5 +22,9 @@ export default function Icon({ name, ...props }: IconProps) {
       return <ProfileIcon {...props} />;
     case "plus":
       return <PlusIcon {...props} />;
+    case "eye":
+      return <EyeIcon {...props} />;
+    case "eye-off":
+      return <EyeOffIcon {...props} />;
   }
 }
