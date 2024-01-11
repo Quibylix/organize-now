647108c7c375b528187b756271eda64c359794c1
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-11 12:19:51 -0600

    feat: allow adding label props to FormField.Label component

--- features/ui/form-field/form-field-label.component.tsx
+++ features/ui/form-field/form-field-label.component.tsx
@@ -1,16 +1,22 @@
+import { joinClassNames } from "@/utils";
 import styles from "./form-field.module.css";
 
-export type FormFieldLabelProps = {
-  label: string;
-  children: React.ReactElement;
-};
+export type FormFieldLabelProps =
+  React.LabelHTMLAttributes<HTMLLabelElement> & {
+    label: string;
+  };
 
 export default function FormFieldLabel({
   label,
   children,
+  className,
+  ...props
 }: FormFieldLabelProps) {
   return (
-    <label className={styles.formFieldLabel}>
+    <label
+      className={joinClassNames(styles.formFieldLabel, className)}
+      {...props}
+    >
       <span>{label}</span>
       {children}
     </label>
