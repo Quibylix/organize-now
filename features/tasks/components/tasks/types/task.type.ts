a25978b5826c9a1ebf8744959af932d253efda6c
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-17 18:03:23 -0600

    fix: update datetime type to Date in Task interface

--- features/tasks/components/tasks/types/task.type.ts
+++ features/tasks/components/tasks/types/task.type.ts
@@ -2,7 +2,7 @@ export type Task = {
   id: number;
   name: string;
   description: string;
-  datetime: string;
+  datetime: Date;
   priority: number;
   category: string;
   status: "uncompleted" | "completed";
