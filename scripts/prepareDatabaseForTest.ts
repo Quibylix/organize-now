d18c2f9cdee4cb6050630a55d38f2b4e275ef028
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-17 19:04:43 -0600

    feat: add mocked tasks and users when preparing the database for testing

--- scripts/prepareDatabaseForTest.ts
+++ scripts/prepareDatabaseForTest.ts
@@ -1,6 +1,8 @@
+import { MOCKED_TASKS } from "@/features/tasks/components/tasks/__mocks__/tasks.mock";
+import { Task } from "@/features/tasks/components/tasks/types/task.type";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
-import pg from "pg";
+import pg, { Client } from "pg";
 
 dotenv.config({ path: ".env.local" });
 
@@ -13,12 +15,24 @@ export async function prepareDatabaseForTest() {
     .connect()
     .catch(() => console.error("Failed to connect to database"));
 
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash("Password1234", saltRounds);
+  await createDatabases(db).catch(err => {
+    console.error("Failed to create databases");
+    console.log(err.message);
+  });
 
-  await db
-    .query(
-      `
+  await insertMockedUsers(db).catch(err => {
+    console.error("Failed to insert mocked users");
+    console.log(err.message);
+  });
+
+  await insertMockedTasks(db, 1).catch(err => {
+    console.error("Failed to insert mocked tasks");
+    console.log(err.message);
+  });
+}
+
+const createDatabases = (db: Client) => {
+  return db.query(`
   DROP TABLE IF EXISTS tasks;
   DROP TABLE IF EXISTS users;
 
@@ -41,12 +55,58 @@ export async function prepareDatabaseForTest() {
     user_id bigint NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
   );
+  `);
+};
 
-  INSERT INTO users (username, hashed_password) VALUES ('Username', '${hashedPassword}');
+const insertMockedUsers = async (db: Client) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash("Password1234", saltRounds);
+
+  return db
+    .query(
+      `
+  INSERT INTO users (username, hashed_password) VALUES
+  ('Username', '${hashedPassword}'),
+  ('Username2', '${hashedPassword}'),
+  ('Username3', '${hashedPassword}'),;
   `,
     )
     .catch(err => {
       console.error("Failed to restart database");
       console.log(err.message);
     });
-}
+};
+
+const insertMockedTasks = (db: Client, userId: number) => {
+  let query =
+    "INSERT INTO tasks (name, description, datetime, priority, category, status, user_id) VALUES ";
+
+  let i = 0;
+  const values: Task[keyof Task][] = [];
+
+  MOCKED_TASKS.forEach((task, index) => {
+    const { name, description, datetime, priority, category, status } = task;
+
+    query += `($${i + 1}, $${i + 2}, $${i + 3}, $${i + 4}, $${i + 5}, $${
+      i + 6
+    }, $${i + 7})`;
+    values.push(
+      name,
+      description,
+      datetime,
+      priority,
+      category,
+      status,
+      userId,
+    );
+    i += 7;
+
+    if (index !== MOCKED_TASKS.length - 1) {
+      query += ", ";
+    }
+  });
+
+  query += ";";
+
+  return db.query(query, values);
+};
