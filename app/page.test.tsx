5a3551fd02ab50b96a5848ee509416826fbfe47f
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-17 18:04:54 -0600

    test: remove unused Home page component test

--- app/page.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
-import Home from "./page";
-
-describe("Home", () => {
-  it("should render the home page", () => {
-    render(<Home />);
-  });
-});
