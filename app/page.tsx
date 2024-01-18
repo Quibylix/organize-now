6a7ca980db0e73ca3621a119db6c5e4094006aad
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-18 09:02:20 -0600

    feat: add ProtectedRoute component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import ProtectedRoute from "@/features/auth/components/protected-route/protected-route";
 import PageWithNavbar from "@/features/layout/components/page-with-navbar/page-with-navbar.component";
 import Tasks from "@/features/tasks/components/tasks/tasks.component";
 
 export default function Home() {
   return (
-    <PageWithNavbar>
-      <Tasks />
-    </PageWithNavbar>
+    <ProtectedRoute>
+      <PageWithNavbar>
+        <Tasks />
+      </PageWithNavbar>
+    </ProtectedRoute>
   );
 }
