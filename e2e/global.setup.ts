45504fcfcb7d402dde186c9015d18e85cc1604ba
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-17 19:50:14 -0600

    chore: configure playwright for emulating a user with no tasks

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -12,6 +12,7 @@ export default async function globalSetup() {
 
   await page.context().storageState({ path: "e2e/.auth/no-user.json" });
 
+  // User with tasks
   await page.goto("/login");
 
   await page.getByLabel("Username").fill("Username");
@@ -22,6 +23,20 @@ export default async function globalSetup() {
   await expect(page).toHaveURL("/");
 
   await page.context().storageState({ path: "e2e/.auth/user.json" });
+
+  // User without tasks
+  await page.goto("/login");
+
+  await page.getByLabel("Username").fill("Username2");
+  await page.getByLabel("Password", { exact: true }).fill("Password1234");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page).toHaveURL("/");
+
+  await page
+    .context()
+    .storageState({ path: "e2e/.auth/user-without-tasks.json" });
 
   await browser.close();
 }
