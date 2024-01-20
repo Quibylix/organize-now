ca2218d521baba3b5d7d1e8b44a1966d4a632b79
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-20 16:15:14 -0600

    feat: add Input component

---
+++ features/ui/components/input/input.component.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { joinClassNames } from "@/utils/join-class-names/join-class-names.util";
+import { useId, useRef, useState } from "react";
+import Icon from "../icon/icon.component";
+import styles from "./input.module.css";
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  startContent?: React.ReactNode;
+  endContent?: React.ReactNode;
+  error?: string;
+};
+
+export default function Input({
+  label,
+  startContent,
+  endContent,
+  error,
+  id: customId,
+  className: customClassName,
+  onFocus,
+  onBlur,
+  ...props
+}: InputProps) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const id = useId();
+
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(true);
+    onFocus?.(event);
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(false);
+    onBlur?.(event);
+  };
+
+  const handleContainerClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.target === e.currentTarget) {
+      inputRef.current?.focus();
+    }
+  };
+
+  const inputId = customId || id;
+  const className = joinClassNames(styles.input, customClassName);
+  const inputContainerClassName = joinClassNames(styles.inputContainer, {
+    [styles.inputContainerFocused]: isFocused,
+  });
+
+  return (
+    <div>
+      {label && (
+        <label className={styles.label} htmlFor={inputId}>
+          {label}
+        </label>
+      )}
+      <div onClick={handleContainerClick} className={inputContainerClassName}>
+        {startContent}
+        <input
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          id={inputId}
+          className={className}
+          ref={inputRef}
+          {...props}
+        />
+        {endContent}
+      </div>
+      {error && (
+        <p role="alert" className={styles.error}>
+          {error}
+        </p>
+      )}
+    </div>
+  );
+}
+
+export type PasswordInputProps = Omit<InputProps, "type"> & {};
+
+Input.Password = function PasswordInput({ endContent, ...props }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(prev => !prev);
+  };
+
+  return (
+    <Input
+      type={showPassword ? "text" : "password"}
+      endContent={
+        <>
+          <button
+            className={styles.showPasswordButton}
+            onClick={toggleShowPassword}
+            aria-label="Toggle password visibility"
+            type="button"
+          >
+            <Icon
+              className={styles.showPasswordIcon}
+              name={showPassword ? "eye-off" : "eye"}
+            />
+          </button>
+          {endContent}
+        </>
+      }
+      {...props}
+    />
+  );
+};
---
+++ features/ui/components/input/input.test.tsx
@@ -0,0 +1,93 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import Input from "./input.component";
+
+describe("Input", () => {
+  it("should render an input field", () => {
+    render(<Input />);
+
+    screen.getByRole("textbox");
+  });
+
+  it("should render a label if provided", () => {
+    render(<Input label="Username" />);
+
+    screen.getByLabelText("Username");
+  });
+
+  it("should render start content if provided", () => {
+    render(<Input startContent={<span>Start</span>} />);
+
+    screen.getByText("Start");
+  });
+
+  it("should render end content if provided", () => {
+    render(<Input endContent={<span>End</span>} />);
+
+    screen.getByText("End");
+  });
+
+  it("should call onFocus callback when input is focused", () => {
+    const onFocusMock = vi.fn();
+    render(<Input onFocus={onFocusMock} />);
+
+    const inputElement = screen.getByRole("textbox");
+    fireEvent.focus(inputElement);
+
+    expect(onFocusMock).toHaveBeenCalled();
+  });
+
+  it("should call onBlur callback when input is blurred", () => {
+    const onBlurMock = vi.fn();
+    render(<Input onBlur={onBlurMock} />);
+
+    const inputElement = screen.getByRole("textbox");
+    fireEvent.blur(inputElement);
+
+    expect(onBlurMock).toHaveBeenCalled();
+  });
+
+  it("should apply custom className to the input element", () => {
+    render(<Input className="custom-input" />);
+
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement.classList).toContain("custom-input");
+  });
+
+  it("should display error message if error prop is provided", () => {
+    render(<Input error="Invalid input" />);
+
+    screen.getByRole("alert", { name: "Invalid input" });
+  });
+});
+
+describe("Input.Password", () => {
+  it("should render a password input field", () => {
+    render(<Input.Password />);
+
+    const inputElement = screen.getByRole("textbox");
+    expect(inputElement.getAttribute("type")).toBe("password");
+  });
+
+  it("should toggle password visibility when show password button is clicked", () => {
+    render(<Input.Password />);
+
+    const showPasswordButton = screen.getByLabelText(
+      "Toggle password visibility",
+    );
+    const inputElement = screen.getByRole("textbox");
+
+    fireEvent.click(showPasswordButton);
+    expect(inputElement.getAttribute("type")).toBe("text");
+
+    fireEvent.click(showPasswordButton);
+    expect(inputElement.getAttribute("type")).toBe("password");
+  });
+
+  it("should render end content along with show password button", () => {
+    render(<Input.Password endContent={<span>End</span>} />);
+
+    screen.getByText("End");
+    screen.getByLabelText("Toggle password visibility");
+  });
+});
