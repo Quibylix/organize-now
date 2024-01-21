bc90cf987829aabf3ffa4d20493825443ac3c24e
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-21 15:35:35 -0600

    fix: add wrapper div for page content to adjust styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import PageWithNavbar from "@/features/layout/components/page-with-navbar/page-w
 import TaskSearchBar from "@/features/tasks/components/tasks-search-bar/tasks-search-bar.component";
 import Tasks from "@/features/tasks/components/tasks/tasks.component";
 import { searchParamsToTasksFilters } from "@/features/tasks/components/tasks/utils/search-params-to-tasks-filters/search-params-to-tasks-filters.util";
+import styles from "./page.module.css";
 
 export type HomeProps = {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -12,8 +13,10 @@ export default function Home({ searchParams }: HomeProps) {
   return (
     <ProtectedRoute>
       <PageWithNavbar>
-        <TaskSearchBar />
-        <Tasks filters={searchParamsToTasksFilters(searchParams)} />
+        <div className={styles.wrapper}>
+          <TaskSearchBar />
+          <Tasks filters={searchParamsToTasksFilters(searchParams)} />
+        </div>
       </PageWithNavbar>
     </ProtectedRoute>
   );
