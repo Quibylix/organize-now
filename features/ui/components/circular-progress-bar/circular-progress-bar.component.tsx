122f409d460d73999fc36d183c278551112d3deb
Vanaldito <barraza.fredimanuel@gmail.com>
2024-01-28 20:50:32 -0600

    feat: add color customization to CircularProgressBar

--- features/ui/components/circular-progress-bar/circular-progress-bar.component.tsx
+++ features/ui/components/circular-progress-bar/circular-progress-bar.component.tsx
@@ -1,19 +1,27 @@
+import { joinClassNames } from "@/utils/join-class-names/join-class-names.util";
 import styles from "./circular-progress-bar.module.css";
 
 export type CircularProgressProps = {
   progress: number;
+  color?: "primary" | "secondary" | "success" | "danger" | "warning";
 };
 
 export default function CircularProgressBar({
   progress,
+  color = "primary",
 }: CircularProgressProps) {
+  const className = joinClassNames(
+    styles.circularProgress,
+    styles[`circularProgressColor__${color}`],
+  );
+
   return (
     <svg
       width="250"
       height="250"
       viewBox="0 0 250 250"
       style={{ "--progress": progress } as React.CSSProperties}
-      className={styles.circularProgress}
+      className={className}
     >
       <circle
         cx="125"
