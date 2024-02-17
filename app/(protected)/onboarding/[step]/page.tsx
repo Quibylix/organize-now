e2fb27b387af7290aa365a2239e40e34f952b3d1
Vanaldito <barraza.fredimanuel@gmail.com>
2024-02-17 10:48:29 -0600

    fix: fix dynamicParams bug and handle step not found

--- app/(protected)/onboarding/[step]/page.tsx
+++ app/(protected)/onboarding/[step]/page.tsx
@@ -1,17 +1,25 @@
 import OnboardingStep from "@/features/onboarding/components/onboarding-step/onboarding-step.component";
+import { notFound } from "next/navigation";
 import styles from "./page.module.css";
 
 export const generateStaticParams = async () => {
   return [{ step: "1" }, { step: "2" }, { step: "3" }];
 };
 
-export const dynamicParams = false;
+// In production mode, dynamicParams set to false does not show 404 pages for unknown paths
+// See https://github.com/vercel/next.js/issues/52765
+// export const dynamicParams = false;
 
 export default function OnboardingPage({
   params: { step },
 }: {
   params: { step: "1" | "2" | "3" };
 }) {
+  // While dynamicParams bug is not fixed, we need to check manually for the step
+  if (!["1", "2", "3"].includes(step)) {
+    notFound();
+  }
+
   return (
     <main className={styles.page}>
       <OnboardingStep step={+step as 1 | 2 | 3} />
